58d54a484a5900b577344d2c2c08ad5a95f83cea
shivatejapecheti <teja79933@gmail.com>
2025-04-28 04:45:01 +0000

    angular models created successfully.

---
+++ angularapp/src/app/models/event.model.ts
@@ -0,0 +1,12 @@
+export interface Event 
+{
+    EventId?: number;
+    Title: string;
+    Description: string;
+    Location: string;
+    Date: Date;
+    OrganizerName: string;
+    ContactInfo: string;
+    PostedDate: Date;
+    Status: string;
+  }
