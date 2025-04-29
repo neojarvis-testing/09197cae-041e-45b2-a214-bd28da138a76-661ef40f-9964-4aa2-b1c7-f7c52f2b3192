4d5948f82eaa1717326f54e22f4716b019660bdd
Ragul7125 <ragularul77@gmail.com>
2025-04-29 11:13:30 +0000

    add event and edit event

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { UserViewEventComponent } from './components/user-view-event/user-view-e
 import { AdminAddEventComponent } from './components/admin-add-event/admin-add-event.component';
 import { AdminViewEventComponent } from './components/admin-view-event/admin-view-event.component';
 const routes: Routes = [
-  {path:"",component:AdminViewEventComponent}
+  {path:"",component:AdminAddEventComponent}
 ];
 
 @NgModule({
