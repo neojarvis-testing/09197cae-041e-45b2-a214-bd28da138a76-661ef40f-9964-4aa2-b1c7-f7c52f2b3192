521c1691443a01f89a8a9c236080d1a29d1fbd3a
Ragul7125 <ragularul77@gmail.com>
2025-04-29 07:23:05 +0000

    admin add and view component

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserViewEventComponent } from './components/user-view-event/user-view-event.component';
 import { AdminAddEventComponent } from './components/admin-add-event/admin-add-event.component';
+import { AdminViewEventComponent } from './components/admin-view-event/admin-view-event.component';
 const routes: Routes = [
-  {path:"",component:AdminAddEventComponent}
+  {path:"",component:AdminViewEventComponent}
 ];
 
 @NgModule({
