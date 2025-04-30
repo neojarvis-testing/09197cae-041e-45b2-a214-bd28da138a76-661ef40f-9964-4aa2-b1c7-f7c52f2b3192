543d812e621dd298a8c6d05951c67e92a18d8c6d
PYasaswini25 <yasaswinipothuraju@gmail.com>
2025-04-30 11:31:55 +0000

    user add feedback

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { RegistrationComponent } from './components/registration/registration.component'; 
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserAddFeedbackComponent } from './components/user-add-feedback/user-add-feedback.component';
  
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'register', component: RegistrationComponent },
-  { path: 'login', component: LoginComponent },
-  {path: '',component:NavbarComponent}
+  // { path: '', component: HomeComponent },
+  // { path: 'register', component: RegistrationComponent },
+  // { path: 'login', component: LoginComponent },
+  // {path: '',component:NavbarComponent},
+  {path:'',component:UserAddFeedbackComponent}
 ];
  
 @NgModule({
